92269a6ccc6e2708abf0f544533a618b826111d5
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-11 20:27:05 +0600

    implement appointment navigation and screens in MainNavGraph

--- app/src/main/java/com/kolu/mediconnect/navigation/MainNavGraph.kt
+++ app/src/main/java/com/kolu/mediconnect/navigation/MainNavGraph.kt
@@ -1,21 +1,32 @@
 package com.kolu.mediconnect.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.kolu.mediconnect.presentation.screens.appointment.AppointmentBookingScreen
+import com.kolu.mediconnect.presentation.screens.appointment.AppointmentDetailsScreen
+import com.kolu.mediconnect.presentation.screens.appointment.AppointmentViewModel
 import com.kolu.mediconnect.presentation.screens.auth.AuthViewModel
 import com.kolu.mediconnect.presentation.screens.auth.login.LoginScreen
 import com.kolu.mediconnect.presentation.screens.auth.register.RegisterScreen
 import com.kolu.mediconnect.presentation.screens.home.HomeScreen
 import org.koin.androidx.compose.koinViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MainNavGraph(modifier: Modifier = Modifier) {
     val navController: NavHostController = rememberNavController()
     val authViewModel: AuthViewModel = koinViewModel()
+    val appointmentViewModel: AppointmentViewModel = koinViewModel()
 
     NavHost(
         navController = navController,
@@ -26,6 +37,9 @@ fun MainNavGraph(modifier: Modifier = Modifier) {
             HomeScreen(
                 onNavigateToLoginScreen = {
                     navController.navigate(DestinationScreens.Login)
+                },
+                onBookAnAppointmentClick = {
+                    navController.navigate(DestinationScreens.AppointmentBooking)
                 }
             )
         }
@@ -40,9 +54,64 @@ fun MainNavGraph(modifier: Modifier = Modifier) {
                 }
             )
         }
-        composable<DestinationScreens.Register> { RegisterScreen(
-            authViewModel = authViewModel,
-            onRegisterSuccess = {navController.navigate(DestinationScreens.Login) }
-        ) }
+        composable<DestinationScreens.Register> {
+            RegisterScreen(
+                authViewModel = authViewModel,
+                onRegisterSuccess = { navController.navigate(DestinationScreens.Login) }
+            )
+        }
+        composable<DestinationScreens.AppointmentBooking> {
+            AppointmentBookingScreen(
+                modifier = Modifier
+                    .fillMaxSize(),
+                viewModel = appointmentViewModel,
+                onAppointmentSuccess = { appointmentId ->
+                    navController.navigate(DestinationScreens.AppointmentDetails(appointmentId)) {
+                        popUpTo(DestinationScreens.Home) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+        composable<DestinationScreens.AppointmentDetails> {
+            val appointmentId = it.arguments?.getString("appointmentId")
+            if (appointmentId != null) {
+                AppointmentDetailsScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    appointmentViewModel = appointmentViewModel,
+                    appointmentId = appointmentId,
+                    onNavigateToHome = {
+                        navController.navigate(DestinationScreens.Home) {
+                            popUpTo(DestinationScreens.Home) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    onNavigateToProfile = {
+                        navController.navigate(DestinationScreens.Profile) {
+                            popUpTo(DestinationScreens.Home) {
+                                inclusive = true
+                            }
+                        }
+                    }
+                )
+            } else {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Appointment ID is null")
+                }
+            }
+        }
+        composable<DestinationScreens.Profile> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Profile Screen")
+            }
+        }
     }
 }
