c86744de75720ad518d66523d7253a3350630a6b
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-11 20:26:05 +0600

    add appointmentModule in MediConnectApplication

--- app/src/main/java/com/kolu/mediconnect/MediConnectApplication.kt
+++ app/src/main/java/com/kolu/mediconnect/MediConnectApplication.kt
@@ -2,7 +2,8 @@ package com.kolu.mediconnect
 
 import android.app.Application
 import com.kolu.mediconnect.di.appModule
-import com.kolu.mediconnect.presentation.screens.auth.di.authModule
+import com.kolu.mediconnect.di.appointmentModule
+import com.kolu.mediconnect.di.authModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -15,7 +16,8 @@ class MediConnectApplication: Application() {
             androidContext(this@MediConnectApplication)
             modules(
                 appModule,
-                authModule
+                authModule,
+                appointmentModule
             )
         }
     }
