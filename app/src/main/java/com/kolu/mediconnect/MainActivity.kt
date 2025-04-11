d088ce7d24480bde9b37e5dce3a2254e62f4e61c
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-11 20:26:49 +0600

    add necessary annotation in MainActivity

--- app/src/main/java/com/kolu/mediconnect/MainActivity.kt
+++ app/src/main/java/com/kolu/mediconnect/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.kolu.mediconnect
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kolu.mediconnect.navigation.MainNavGraph
-import com.kolu.mediconnect.presentation.screens.LoadingScreen
 import com.kolu.mediconnect.ui.theme.MediConnectTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
