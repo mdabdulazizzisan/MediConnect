9b3342b1efbdb993df773161c53df30e708a8dbc
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-16 07:47:31 +0600

    Add auto scrolling carousal in home

--- app/src/main/java/com/kolu/mediconnect/presentation/screens/home/HomeContent.kt
+++ app/src/main/java/com/kolu/mediconnect/presentation/screens/home/HomeContent.kt
@@ -1,16 +1,27 @@
 package com.kolu.mediconnect.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,11 +31,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kolu.mediconnect.R
 import com.kolu.mediconnect.ui.theme.MediConnectTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun HomeContent(
@@ -44,7 +65,9 @@ fun HomeContent(
                 .padding(innerPadding)
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
-        ) { }
+        ) {
+            BannerCarousel(onBannerClick = onBookAnAppointmentClick)
+        }
     }
 }
 
@@ -104,6 +127,83 @@ fun HomeTopBar(
 
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun BannerCarousel(
+    modifier: Modifier = Modifier,
+    onBannerClick: () -> Unit
+) {
+    val bannerImages = listOf(
+        R.drawable.image1,
+        R.drawable.image4,
+        R.drawable.image2,
+        R.drawable.image3
+
+    )
+    
+    val pagerState = rememberPagerState(pageCount = { bannerImages.size })
+    
+    LaunchedEffect(Unit) {
+        while(true) {
+            delay(3000)
+            val nextPage = (pagerState.currentPage + 1) % bannerImages.size
+            pagerState.animateScrollToPage(nextPage)
+        }
+    }
+    
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier.fillMaxSize()
+            ) { page ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp)
+                        .clickable { onBannerClick() },
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = bannerImages[page]),
+                        contentDescription = "Banner ${page + 1}",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+            
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 8.dp)
+            ) {
+                repeat(bannerImages.size) { index ->
+                    val isSelected = pagerState.currentPage == index
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 4.dp)
+                            .width(if (isSelected) 24.dp else 12.dp)
+                            .height(8.dp)
+                            .clip(CircleShape)
+                            .background(
+                                if (isSelected) MaterialTheme.colorScheme.primary
+                                else MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
